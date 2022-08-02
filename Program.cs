6c5d3314dd03714593fbb310b8d2884f474a7d6c
SahinZambak <sahinzambak7@gmail.com>
2022-08-02 13:16:54 +0300

    first

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ucgen_Cizim
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Üçgenin kenar uzunluğu ne olsun?: ");
+            int kenarUzunluk = Convert.ToInt32(Console.ReadLine());
+            for (int i = 1; i <= kenarUzunluk; i++)
+            {
+                for (int j = 0; j < kenarUzunluk - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int k = 0; k < 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
